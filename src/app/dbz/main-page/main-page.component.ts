5b25af1200c0d1dc04a52c248bc5fec089c6d2c8
davidShum <david980531@gmail.com>
2024-02-29 12:34:41 +0100

    curso hasta primera clase de servicios

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
+import { DbzService } from './../services/dbz.service';
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
-interface Personaje {
-  nombre: string,
-  poder : number
-}
+// interface Personaje {
+//   nombre: string,
+//   poder : number
+// }
 
 @Component({
   selector: 'app-main-page',
@@ -12,13 +14,45 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: "Naruto",
+      poder: 1500
+    },
+  {
+    nombre: "Sasuke",
+    poder: 1450
+  },
+  {
+    nombre: "Krillen",
+    poder: 700
+  },
+  {
+    nombre: "Goku",
+    poder: 15000
+  },
+  {
+    nombre: "Vegeta",
+    poder: 8500
+  }
+  ];
+
   nuevo: Personaje = {
-    nombre: "Trucks",
-    poder: 14000
+    nombre: "",
+    poder: 0
   }
 
   agregar(){
+    if(this.nuevo.nombre.trim().length === 0){ return; }
     console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: "",
+      poder: 0
+    }
   }
+
+  constructor(private DbzService: DbzService){}
 
 }
